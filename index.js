84d7725a29a7443ef3e30c1f24b27b25250807c1
Zulai Romero <zulairomero@gmail.com>
2023-07-02 22:05:13 -0400

    Adds palindrome code solution

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  if (word === [...word].reverse().join(""))
+  return true;
+  else return (false)
+};
 
-/* 
+console.log(isPalindrome("dad"))
+
+/* /home/seb_slack_91/Development/code/phase-1/phase-1-algorithms-palindrome/test
   Add your pseudocode here
+  function isPalindrome (paramneter) {
+    if parameter is === reverse string of paramter, return true,
+    else return false
+  }
 */
 
 /*
   Add written explanation of your solution here
+  Created an if else statement, the if part applies 3 operations on the paramter to verify whether the string is the same in revers aka a Palindrome. The else statement is the catch all for if the statement is false, aka not a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
